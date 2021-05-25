11360110a84381d04f64ad2feb0f7aa06683c8ed
ttuulari@gmail.com <ttuulari@gmail.com>
2021-05-25 12:46:22 +0300

    deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/emil0r/ez-mixpanel"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [commons-codec/commons-codec "1.10"]
-                 [http-kit "2.1.19"]
+                 [http-kit "2.5.3"]
                  [cheshire "5.3.1"]])
